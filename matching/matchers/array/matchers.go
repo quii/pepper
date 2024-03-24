b1ef6b27bd43b9cc5a6e8e8b54391374d980931d
Chris James <quii@hey.com>
2024-03-24 17:18:27 +0000

    array matching using matchers

--- matching/matchers/array/matchers.go
+++ matching/matchers/array/matchers.go
@@ -1,24 +1,25 @@
 package array
 
-import (
-	"fmt"
-	"github.com/quii/pepper/matching"
-)
+import "github.com/quii/pepper/matching"
 
-func Contain[T comparable](needle T) matching.Matcher[[]T] {
-	return func(haystack []T) matching.MatchResult {
-		for _, h := range haystack {
-			if h == needle {
+func ContainItem(m matching.Matcher[string]) matching.Matcher[[]string] {
+	return func(items []string) matching.MatchResult {
+		var exampleFailure matching.MatchResult
+		for _, item := range items {
+			result := m(item)
+			if result.Matches {
 				return matching.MatchResult{
-					Description: fmt.Sprintf("contain %v", needle),
+					Description: "contain item",
 					Matches:     true,
 				}
+			} else {
+				exampleFailure = result
 			}
 		}
 
-		return matching.MatchResult{
-			Description: fmt.Sprintf("contain %v", needle),
-			But:         "it did not",
-		}
+		exampleFailure.But = "it did not"
+		exampleFailure.Description = "contain item " + exampleFailure.Description
+
+		return exampleFailure
 	}
 }
--- matching/matchers/array/matchers_test.go
+++ matching/matchers/array/matchers_test.go
@@ -2,20 +2,29 @@ package array
 
 import (
 	. "github.com/quii/pepper/matching"
+	. "github.com/quii/pepper/matching/matchers/comparable"
 	. "github.com/quii/pepper/matching/matchers/spytb"
+	. "github.com/quii/pepper/matching/matchers/string"
 	"testing"
 )
 
 func TestArrayMatchers(t *testing.T) {
-	t.Run("Contain comparable", func(t *testing.T) {
+	t.Run("contain with other matcher to find matcher", func(t *testing.T) {
 		t.Run("passing", func(t *testing.T) {
-			Expect(t, []int{1, 2, 3}).To(Contain(2))
+			Expect(t, []string{"hello", "WORLD"}).To(ContainItem(HaveAllCaps))
 		})
 
 		t.Run("failing", func(t *testing.T) {
-			spyTB := &SpyTB{}
-			Expect(spyTB, []int{1, 2, 3}).To(Contain(4))
-			Expect(t, spyTB).To(HaveError("expected [1 2 3] to contain 4, but it did not"))
+			t.Run("equal to", func(t *testing.T) {
+				spyTB := &SpyTB{}
+				Expect(spyTB, []string{"hello", "world"}).To(ContainItem(EqualTo("goodbye")))
+				Expect(t, spyTB).To(HaveError(`expected [hello world] to contain item be equal to goodbye, but it did not`))
+			})
+			t.Run("all caps", func(t *testing.T) {
+				spyTB := &SpyTB{}
+				Expect(spyTB, []string{"hello", "world"}).To(ContainItem(HaveAllCaps))
+				Expect(t, spyTB).To(HaveError(`expected [hello world] to contain item be in all caps, but it did not`))
+			})
 		})
 	})
 }
